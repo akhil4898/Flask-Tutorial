27402623ff7ded8c4a14d70c4e89d88fec946788
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-28 15:21:27 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+# Flask Tutorial...
+
+
+from flask import Flask, render_template        # Import the Flask module from flask...
+
+# Here, __name__ is the name of current module...
+
+# Here Flask is a constructor & __name__ as the argument into the Flask constructor....
+
+#An object (app) of the Flask class is considered as the WSGI application.
+
+app = Flask(__name__)               #Creating the Flask class object...So, Here app is the object...
+
+# The route() function of the Flask class defines the URL mapping of the associated (related) function.
+
+@app.route('/')                     # decorator defines the
+def hello_world():                 
+    return render_template('index.html')                   # render_template always used with return statement.
+    # return 'Hello world! This is Akhil Upadhyay'
+
+@app.route('/products')
+def product():
+    return "This is my product's hi page"
+
+
+# Finally, the run method of the Flask class is used to run the flask application on the local development server.
+if __name__ == '__main__':
+        app.run(debug=True, port=8000)
