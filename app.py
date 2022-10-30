5067e2a8ded82a92243742b88f5fccfaeab1feae
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-30 11:13:05 +0530

    Update file

--- app.py
+++ app.py
@@ -1,37 +1,35 @@
-# # # Flask Tutorial...
+# # Flask Tutorial...
 
 
-# # from flask import Flask, render_template        # Import the Flask module from flask...
+# from flask import Flask, render_template        # Import the Flask module from flask...
 
-# # # Here, __name__ is the name of current module...
+# # Here, __name__ is the name of current module...
 
-# # # Here Flask is a constructor & __name__ as the argument into the Flask constructor....
+# # Here Flask is a constructor & __name__ as the argument into the Flask constructor....
 
-# # #An object (app) of the Flask class is considered as the WSGI application.
+# #An object (app) of the Flask class is considered as the WSGI application.
 
-# # app = Flask(__name__)               #Creating the Flask class object...So, Here app is the object...
+# app = Flask(__name__)               #Creating the Flask class object...So, Here app is the object...
 
-# # # The route() function of the Flask class defines the URL mapping of the associated (related) function.
+# # The route() function of the Flask class defines the URL mapping of the associated (related) function.
 
-# # # @app.route('/home/<int:age>')                     # decorator defines the
-# # # def home(age):                 
-# #     # return "Your age is: " + str(age)
-# #     # return 'Hello world! This is Akhil Upadhyay'
+# # @app.route('/home/<int:age>')                     # decorator defines the
+# # def home(age):                 
+#     # return "Your age is: " + str(age)
+#     # return 'Hello world! This is Akhil Upadhyay'
     
-# # @app.route("/")
-# # def home():
-# #     return render_template('index.html')                   # render_template always used with return statement.
+# @app.route("/")
+# def home():
+#     return render_template('index.html')                   # render_template always used with return statement.
 
-# # @app.route('/products')
-# # def product():
-# #     return "This is my product's hi page"
-
-
-# # # Finally, the run method of the Flask class is used to run the flask application on the local development server.
-# # if __name__ == '__main__':
-# #         app.run(debug=True, port=8000)
+# @app.route('/products')
+# def product():
+#     return "This is my product's hi page"
 
 
+# # Finally, the run method of the Flask class is used to run the flask application on the local development server.
+# if __name__ == '__main__':
+#         app.run(debug=True, port=8000)
 
 
 
@@ -39,35 +37,36 @@
 
 
 
-# # from flask import redirect
+
+
+# from flask import redirect
 # from flask import Flask, render_template, redirect, url_for
 
 # app = Flask(__name__)
 
 
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
+# def about():
+#     return "This is about page..."
 
-# # @app.route('/')
-# # def home():
-# #     return render_template('index.html')
-
-
-
-
-# # def about():
-# #     return "This is about page..."
-
-# # def viewThis():
-# #     return "This is very good city..."
+# def viewThis():
+#     return "This is very good city..."
 
 # # Another approach to perform routing for the flask web application
-# # app.add_url_rule('/about', "city", viewThis)
-# # app.add_url_rule('/about', "about", about)
+# app.add_url_rule('/viewThis', "city", viewThis)
+# app.add_url_rule('/about', "about", about)
 
 
 
 
 
+
+# @app.route('/')
+# def home():
+#     return "Welcome to my first page!"
 
 # @app.route('/admin')
 # def admin():
@@ -90,8 +89,51 @@
 #        return redirect(url_for('librarian'))
 #     if name == 'student':
 #        return redirect(url_for('student'))
-   
 
 
-# if __name__ == '__main__':
-#     app.run(port=8080, debug=True)
+
+
+
+
+
+# Databasde connection..
+
+# from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy  
+# from datetime import datetime
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///akhil.db"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+# db = SQLAlchemy(app)
+
+ 
+# class Todo(db.Model):
+#     sno = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(200), nullable=False)
+#     desc = db.Column(db.String(500), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+#     def __repr__(self) -> str:
+#         return f"{self.sno} - {self.title}"
+
+
+# @app.route('/')
+# def home():
+#     todo = Todo(title="First todo", desc="Start investing in stock market...")
+#     db.session.add(todo)
+#     db.session.commit()
+#     return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
